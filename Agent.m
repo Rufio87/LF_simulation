7c25ed8dd270800d29d01e058ae6328a0bb3a73d
Rufio87 <l_moeller@gmx.at>
2015-05-18 16:29:03 +0200

    Class Agent added

---
+++ Agent.m
@@ -0,0 +1,16 @@
+classdef Agent
+    %UNTITLED6 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        A;
+        B;
+        x0;
+        isLeader;
+    end
+    
+    methods
+    end
+    
+end
+
